4ce867c13c4b83c10fc8d9e55d4c3b8ffd4be177
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-12 18:14:11 +0000

    enabled viewBinding in project

--- app/src/main/java/com/codelab/samsungcalcluator/MainActivity.kt
+++ app/src/main/java/com/codelab/samsungcalcluator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.codelab.samsungcalcluator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codelab.samsungcalcluator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // viewBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }
