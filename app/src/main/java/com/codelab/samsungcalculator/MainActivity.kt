91e886f016b2c6e7991608cf5e4db97cf63f7f50
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-20 23:38:15 +0000

    new

--- app/src/main/java/com/codelab/samsungcalculator/MainActivity.kt
+++ app/src/main/java/com/codelab/samsungcalculator/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.codelab.samsungcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
